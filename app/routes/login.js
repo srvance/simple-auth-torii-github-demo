9da6b186c14880163d64d023af957ae45c913564
Stephen Vance <steve@vance.com>
2016-12-17 16:40:03 -0500

    Add the first step in the authentication, obtaining the authorization code from GitHub.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  actions: {
+    login() {
+      this.get('session').authenticate('authenticator:torii', 'github');
+    }
+  }
 });
