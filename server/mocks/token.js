5edfb78e9a7bbfcdc187674920d422f6d93f4552
Stephen Vance <steve@vance.com>
2016-12-19 14:29:45 -0500

    Add an http-mock for the token server rather than relying on an externally hosted server for development.

---
+++ server/mocks/token.js
@@ -0,0 +1,57 @@
+/*jshint node:true*/
+module.exports = function(app) {
+  const express = require('express');
+  const tokenRouter = express.Router();
+  const https = require('https');
+  const settings = require('../settings');
+
+  tokenRouter.post('/', function(req, res) {
+    const body = req.body;
+
+    const payload = {
+      'client_id': settings.CLIENT_ID,
+      'client_secret': settings.CLIENT_SECRET,
+      'code': body.authorizationCode
+    };
+    if (body.state) {
+      payload.state = body.state;
+    }
+
+    const data = JSON.stringify(payload);
+
+    const options = {
+      hostname: 'github.com',
+      port: 443,
+      path: '/login/oauth/access_token',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(data),
+        'Accept': 'application/json',
+        'User-Agent': settings.USER_AGENT
+      }
+    };
+
+    const ghReq = https.request(options, (ghRes) => {
+      let body = '';
+      ghRes.setEncoding('utf8');
+      ghRes.on('data', (chunk) => body += chunk);
+      ghRes.on('end', () => {
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        });
+        res.write(JSON.stringify(body));
+        res.end();
+      });
+    });
+    ghReq.on('error', (error) => {
+      console.error(error);
+      res.status(500).end();
+    });
+    ghReq.write(data);
+    ghReq.end();
+  });
+
+  app.use('/api/token', require('body-parser').json());
+  app.use('/api/token', tokenRouter);
+};
