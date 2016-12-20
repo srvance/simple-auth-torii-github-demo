9682ff4113b68667a32da818689e788729b8b2f3
Stephen Vance <steve@vance.com>
2016-12-20 12:04:30 -0500

    Explicitly inject the session service. Auto-inject is too magical and will go away.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
   config: config.torii.providers['github-oauth2'],
   actions: {
     logout() {
